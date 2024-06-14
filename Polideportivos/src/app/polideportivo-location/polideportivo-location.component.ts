49938601c7bc3592dbd641fa167d50ae8193df04
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-06-14 20:26:14 +0200

    Agregar un enlace de propiedad, interpolación

--- Polideportivos/src/app/polideportivo-location/polideportivo-location.component.ts
+++ Polideportivos/src/app/polideportivo-location/polideportivo-location.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Polideportivolocation } from '../polideportivolocation';
 
 @Component({
   selector: 'app-polideportivo-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      polideportivo-location works!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="polideportivoLocation.photo" alt="Exterior photo of {{polideportivoLocation.name}}">
+    <h2 class="listing-heading">{{ polideportivoLocation.name }}</h2>
+    <p class="listing-location">{{ polideportivoLocation.city}}, {{polideportivoLocation.state }}</p>
+  </section>
   `,
   styleUrl: './polideportivo-location.component.css'
 })
 export class PolideportivoLocationComponent {
-
+  @Input() polideportivoLocation! : Polideportivolocation;
 }
--- Polideportivos/src/app/polideportivo/polideportivo.component.ts
+++ Polideportivos/src/app/polideportivo/polideportivo.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PolideportivoLocationComponent } from '../polideportivo-location/polideportivo-location.component';
+import { Polideportivolocation } from '../polideportivolocation';
 
 @Component({
   selector: 'app-polideportivo',
   standalone: true,
   imports: [
-    PolideportivoComponent,
+    CommonModule,
+    PolideportivoLocationComponent
   ],
   template: `
   <section>
     <form>
       <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Buscar</button>
+      <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-polideportivo-location [polideportivoLocation]="polideportivolocation"]></app-polideportivo-location>
+  </section>
+  `,
   styleUrl: './polideportivo.component.css'
 })
 export class PolideportivoComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  polideportivolocation: Polideportivolocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
