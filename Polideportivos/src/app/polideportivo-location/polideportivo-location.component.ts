7576d5fbfd4ef385658fe5b5f342042eca120e69
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-06-14 19:10:57 +0200

    crea una nueva interfaz

---
+++ Polideportivos/src/app/polideportivo-location/polideportivo-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-polideportivo-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      polideportivo-location works!
+    </p>
+  `,
+  styleUrl: './polideportivo-location.component.css'
+})
+export class PolideportivoLocationComponent {
+
+}
