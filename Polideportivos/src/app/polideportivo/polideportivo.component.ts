098d2bbbe932dc88c6ce9cdc9e17efe5c452ff02
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-06-14 17:58:00 +0200

    Proyecto Creado

---
+++ Polideportivos/src/app/polideportivo/polideportivo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-polideportivo',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      polideportivo works!
+    </p>
+  `,
+  styleUrl: './polideportivo.component.css'
+})
+export class PolideportivoComponent {
+
+}
