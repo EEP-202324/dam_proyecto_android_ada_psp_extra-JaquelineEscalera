d9e992d4343d27d1e1c5cd06255fe1ad0df7d07c
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-06-15 00:01:05 +0200

    Spring creado

---
+++ Polideportivos Spring/Polideportivo/src/main/java/com/example/Polideportivo/PolideportivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Polideportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolideportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolideportivoApplication.class, args);
+	}
+
+}
---
+++ Polideportivos Spring/Polideportivo/src/test/java/com/example/Polideportivo/PolideportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Polideportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolideportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
